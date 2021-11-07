870dc09039ea17f55b7d452c07769cae4c292197
Vitalii Shapovalov <vitalii.shapovalov@vimeo.com>
2021-11-07 10:03:32 +0200

    change test runner from tap to ava

--- test/return-types.test.js
+++ test/return-types.test.js
@@ -1,4 +1,4 @@
-const test = require("tap").test;
+const test = require("ava");
 
 const mediaDevicesUtil = require("..");
 
@@ -8,36 +8,33 @@ const isMac = process.platform === "darwin";
 
 const verifyDeviceObject = (t, device) => {
     // device itself should always be an object
-    t.type(device, "object");
+    t.is(typeof device, "object");
 
     // CI will have no devices at all, so we're expecting empty results
     const deviceFieldType = isCI ? "undefined" : "string";
-    t.type(device.label, deviceFieldType);
+    t.is(typeof device.label, deviceFieldType);
     // windows doesn't have an ID, only alternative name
     if (isWin) {
-        t.type(device.alternativeName, deviceFieldType);
+        t.is(typeof device.alternativeName, deviceFieldType);
     } else {
-        t.type(device.id, deviceFieldType);
+        t.is(typeof device.id, deviceFieldType);
     }
 };
 
 if (isMac) {
     test("getDefaultVideoDevice - should return 'Device' object", (t) => {
         verifyDeviceObject(t, mediaDevicesUtil.getDefaultVideoDevice());
-        t.end();
     });
 }
 
 if (isWin) {
     test("getDefaultVideoDevice - should return null", (t) => {
-        t.type(mediaDevicesUtil.getDefaultVideoDevice(), "null");
-        t.end();
+        t.is(mediaDevicesUtil.getDefaultVideoDevice(), null);
     });
 }
 
 test("getDefaultAudioDevice - should return 'Device' object", (t) => {
     verifyDeviceObject(t, mediaDevicesUtil.getDefaultAudioDevice());
-    t.end();
 });
 
 test("getVideoDevices - should return an Array of 'Device' objects", (t) => {
@@ -46,7 +43,6 @@ test("getVideoDevices - should return an Array of 'Device' objects", (t) => {
     for (const videoDevice of videoDevices) {
         verifyDeviceObject(t, videoDevice);
     }
-    t.end();
 });
 
 test("getAudioDevices - should return an Array of 'Device' objects", (t) => {
@@ -55,5 +51,4 @@ test("getAudioDevices - should return an Array of 'Device' objects", (t) => {
     for (const audioDevice of audioDevices) {
         verifyDeviceObject(t, audioDevice);
     }
-    t.end();
 });

1a344e26079e7190a7bc7293e21ab2e84794d2a4
Vitali Shapovalov <shapovalov.vitali.v@gmail.com>
2021-11-30 09:39:50 +0200

    adjust tests

--- test/return-types.test.js
+++ test/return-types.test.js
@@ -3,6 +3,7 @@ const test = require("ava");
 const mediaDevicesUtil = require("..");
 
 const isWin = process.platform === "win32";
+const isString = (val) => typeof val === "string";
 
 const verifyDeviceObject = (t, device) => {
     // device itself should always be an object
@@ -40,4 +41,35 @@ test("getAudioDevices - should return an Array of 'Device' objects", (t) => {
     for (const audioDevice of audioDevices) {
         verifyDeviceObject(t, audioDevice);
     }
+});
+
+test("getScreenAuthorizationStatus - should return string as auth status", async (t) => {
+    t.assert(isString(mediaDevicesUtil.getScreenAuthorizationStatus()));
+});
+
+test("getMediaAuthorizationStatus - should return string as auth status (when media category is provided)", (t) => {
+    const cameraPermission = mediaDevicesUtil.getMediaAuthorizationStatus("camera");
+    t.assert(isString(cameraPermission));
+    const microphonePermission = mediaDevicesUtil.getMediaAuthorizationStatus("microphone");
+    t.assert(isString(microphonePermission));
+});
+
+test("getMediaAuthorizationStatus - should return object of strings as auth statuses (when no media category is provided)", (t) => {
+    const permissions = mediaDevicesUtil.getMediaAuthorizationStatus();
+    t.assert(typeof permissions === "object");
+    t.assert(isString(permissions.camera));
+    t.assert(isString(permissions.microphone));
+});
+
+test("requestScreenAuthorization - should return string as auth status", (t) => {
+    t.assert(isString(mediaDevicesUtil.requestScreenAuthorization()));
+});
+
+test("requestMediaAuthorization - should return Promise with string as auth status", async (t) => {
+    const cameraPermissionRequestPromise = mediaDevicesUtil.requestMediaAuthorization("camera");
+    t.assert(cameraPermissionRequestPromise instanceof Promise);
+    t.assert(isString(await cameraPermissionRequestPromise));
+    const microphonePermissionRequestPromise = mediaDevicesUtil.requestMediaAuthorization("microphone");
+    t.assert(microphonePermissionRequestPromise instanceof Promise);
+    t.assert(isString(await microphonePermissionRequestPromise));
 });
